e32ab85b033f74391e2a31d45bd828f3a3c46f34
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-12-20 11:44:48 +0300

    feat: module4 task1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("true");
+        }
     }
 }

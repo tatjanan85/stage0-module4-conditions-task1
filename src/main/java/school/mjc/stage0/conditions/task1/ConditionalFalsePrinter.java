a288e07bc8f764d1002802209ffd57085ff4d94e
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-12-20 11:56:16 +0300

    fix: fix conditions

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
-        if (args.length > 0) {
+        if (-5 > 0) {
             System.out.println("true");
         } else {
             System.out.println("false");
